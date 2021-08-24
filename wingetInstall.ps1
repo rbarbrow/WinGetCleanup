c0fe95f23233f16a49820907b56a6153cc702ba9
rbarbrow <rbarbrow@markonsolutions.com>
2021-08-24 18:25:12 -0400

    Create wingetInstall.ps1

---
+++ wingetInstall.ps1
@@ -0,0 +1,14 @@
+winget install
+winget install Microsoft.WindowsTerminal
+winget install Microsoft.VisualStudioCode
+winget install git.get
+winget install GitHub.GitHubDesktop    
+winget install google.chrome
+winget install Microsoft.Teams
+winget install tomlm.electron-outlook-365
+winget install Microsoft.PowerBI
+
+
+
+
+
